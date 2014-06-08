797f4d570e08f4ef9005ae032abc2dbec6320e3b
justy989 <justint989@gmail.com>
2014-06-08 14:18:14 -0400

    improved driver readability via 'using'
    -improved ns printing by adding commas

--- sort_array/main.cpp
+++ sort_array/main.cpp
@@ -17,8 +17,11 @@ int main()
 
     // generate 50 ints, copy them for best and worst input
     auto average_input = generate_unique<int>( seed, n );
-    decltype(average_input) best_input ( average_input );
-    decltype(average_input) worst_input ( average_input );
+
+    using input = decltype(average_input);
+
+    input best_input { average_input };
+    input worst_input { average_input };
 
     // best input is already sorted
     std::sort( best_input.begin(), best_input.end() );
@@ -28,15 +31,15 @@ int main()
                [](int i, int j){return i > j;} );
 
 
-    using test_case_lambda = test< decltype(average_input) >::case_func;
-    using test_check_lambda = test< decltype(average_input) >::check_func;
+    using test_case_fn = test< input >::case_func;
+    using test_check_fn = test< input >::check_func;
 
     // create our lambdas for each case, capture vectors by copying
-    test_case_lambda is_fn = [](decltype(average_input) input){ insertion_sort(input); return std::move(input); };
-    test_case_lambda ms_fn = [](decltype(average_input) input){ merge_sort(input); return std::move(input); };
+    test_case_fn is_fn = [](input input){ insertion_sort(input); return std::move(input); };
+    test_case_fn ms_fn = [](input input){ merge_sort(input); return std::move(input); };
 
     // create our lambda to check correctness
-    test_check_lambda check_correctness = []( const std::vector<int>& input )
+    test_check_fn check_correctness = []( const std::vector<int>& input )
     {
         for(size_t i = 1; i < input.size(); ++i){
             if( input[i - 1] > input[i] ){
@@ -47,29 +50,29 @@ int main()
         return true;
     };
 
-    using case_info = std::vector< test<decltype(average_input)>::case_info >;
+    using case_info = std::vector< test<input>::case_info >;
 
     // setup case info. Match Case id to input
     case_info is_cases {
-        { "Best Case: O(n)", best_input },
-        { "Average Case: O((n/2)^2)", average_input },
-        { "Worst Case: O(n^2)", worst_input },            
+        { "Best Case: n", best_input },
+        { "Average Case: (n/2)^2", average_input },
+        { "Worst Case: n^2", worst_input },            
     };
 
     case_info ms_cases {
-        { "Average Case: O(n lg n)", average_input },
+        { "Average Case: n lg n", average_input },
     };
     
     // create the tests and run them
-    test<decltype(average_input)> is_test ( "Insertion Sort"s,
-                                            is_fn,
-                                            check_correctness,
-                                            std::move(is_cases) );
+    test<input> is_test { "Insertion Sort"s,
+                          is_fn,
+                          check_correctness,
+                          std::move(is_cases) };
 
-    test<decltype(average_input)> ms_test ( "Merge Sort"s,
-                                            ms_fn,
-                                            check_correctness,
-                                            std::move(ms_cases) );
+    test<input> ms_test { "Merge Sort"s,
+                          ms_fn,
+                          check_correctness,
+                          std::move(ms_cases) };
 
     is_test.run();
     ms_test.run();
--- utils/test.cpp
+++ utils/test.cpp
@@ -1,6 +1,9 @@
 #include "test.hpp"
 
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <locale>
 #include <chrono>
 
 using namespace std;
@@ -19,6 +22,15 @@ utils::test<INPUT>::test( const std::string& name,
 
 }
 
+template < typename T >
+std::string format_ns_with_commas(T count)
+{
+    std::stringstream ss;
+    ss.imbue(std::locale("en_US"));
+    ss << std::fixed << count;
+    return ss.str();
+}
+
 template < typename INPUT >
 void utils::test<INPUT>::run()
 {
@@ -34,7 +46,8 @@ void utils::test<INPUT>::run()
         auto end = high_resolution_clock::now();
 
         cout << "Stop "
-             << duration_cast<nanoseconds>(end - start).count()
+             << format_ns_with_commas( 
+                 duration_cast<nanoseconds>(end - start).count() )
              << " ns" << endl;
 
         // check correctness
